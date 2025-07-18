e3c134298969480fa358d38ce359c0fa89bfddbc
Guilhermebsdourado <guilhermebarsilva@gmail.com>
2025-07-18 06:59:37 -0300

    Arrow Function

---
+++ 04-arrow-functions/index.js
@@ -0,0 +1,14 @@
+// let resultadoDaSoma = (numero1, numero2) =>{
+//     return numero1 + numero2
+// }
+
+    // Forma reduzida
+
+// let resultadoDaSoma = (numero1, numero2) => numero1 + numero2
+
+// console.log(resultadoDaSoma(12, 5))
+
+const incentivarQuester = nomeQuester => 
+    console.log('Parabéns' + nomeQuester + 'você chegou ao módulo de JS intermediário')
+
+incentivarQuester('Guilherme')
