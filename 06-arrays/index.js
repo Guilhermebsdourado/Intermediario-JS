be874712846990139aa069d464ad35f68c7f06ef
Guilhermebsdourado <guilhermebarsilva@gmail.com>
2025-07-18 10:16:16 -0300

    06-Arrays

---
+++ 06-arrays/index.js
@@ -0,0 +1,8 @@
+let filmes = ['Vingadores', 'Mulher Maravilha']
+// let avaliacoes = [1, 4.6, 5]
+
+// console.log(filmes)
+
+let descricaoFilme = ['Vingadores', 4.5, true]
+
+console.log(descricaoFilme)
