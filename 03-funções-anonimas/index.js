cd5af90a0ffa1a4db4b4875eb712ee3215d22249
Guilhermebsdourado <guilhermebarsilva@gmail.com>
2025-07-18 06:44:27 -0300

    Escopo no JS, Hoisting e Funções anonimas

---
+++ 03-funções-anonimas/index.js
@@ -0,0 +1,16 @@
+// jeito antigo de fazer
+
+// function soma(numero1, numero2){
+//     return numero1 + numero2
+// }
+
+// let resultadoDaSoma = somar(12, 5)
+// console.log(resultadoDaSoma)
+
+// Jeito Novo
+
+let resultadoDaSoma = function(numero1, numero2){
+    return numero1 + numero2
+}
+
+console.log(resultadoDaSoma(12, 5))
