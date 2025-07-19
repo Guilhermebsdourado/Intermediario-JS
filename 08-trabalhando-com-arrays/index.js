4ec1e1742840e85e7ffbd43a70e556e3747698c2
Guilhermebsdourado <guilhermebarsilva@gmail.com>
2025-07-19 00:06:27 -0300

    10-Objetos

---
+++ 08-trabalhando-com-arrays/index.js
@@ -0,0 +1,20 @@
+let frutas = ['banana', 'maçã', 'laranja', 'pêra']
+let frutasTamanho = frutas.length
+
+let posicaoLaranja = frutas.indexOf('laranja')
+
+console.log(posicaoLaranja)
+
+
+
+
+
+
+
+// for(let i = 0; i < frutasTamanho; i++){
+//     console.log('Fruta: '+ frutas [i])
+// }
+
+// frutas.forEach(function(item){
+//     console.log('Fruta: ' + item)
+// })
