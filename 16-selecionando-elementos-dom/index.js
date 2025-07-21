a1f2de8bba99d8748c9c8dc298cdc36cb9518cfa
Guilhermebsdourado <guilhermebarsilva@gmail.com>
2025-07-20 22:55:20 -0300

    selecionando elementos do dom

---
+++ 16-selecionando-elementos-dom/index.js
@@ -0,0 +1,17 @@
+// console.log(document)
+
+// document.getElementById('paragrafo 1')
+
+// console
+
+// let inputsPorClasse = document.getElementsByClassName('tag-de-input');
+
+// console.log(inputsPorClasse[0])
+
+// let inputsPorTag = document.getElementsByTagName('h1');
+
+// console.log(inputsPorTag)
+
+let inputPorName = document.querySelectorAll("input[name='email']");
+
+console.log(inputPorName)
